ed9e0893f74e9156ae48df32ff6111234d1ee50f
Semen Kotlyar <kotlyarsemen@gmail.com>
2023-01-13 11:56:18 +0200

    .

--- src/heap/tester.java
+++ src/heap/tester.java
@@ -1,8 +1,11 @@
 package heap;
 
+import java.io.Console;
+
 public class tester {
 
 	public static void main(String[] args) {
 		FibonacciHeap f = new FibonacciHeap();
+		System.out.println(".");
 	}
 }
